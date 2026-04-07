24bbd568131a658f53da76f884bda836f3743e48
Gabriel Landais <gabriel.landais@gmail.com>
2026-04-07 10:22:49 +0200

    fix: missing public unlisted asset endpoint

---
+++ backend/src/main/java/fr/pedalons/api/assets/DownloadPublicUnlistedAssetResource.java
@@ -0,0 +1,6 @@
+package fr.pedalons.api.assets;
+
+import jakarta.ws.rs.Path;
+
+@Path("/api/download/public_unlisted")
+public class DownloadPublicUnlistedAssetResource extends AbstractDownloadAssetResource {}
