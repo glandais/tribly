d7903387885fb05eec02a96a2de43ab46c03a085
Gabriel Landais <gabriel.landais@gmail.com>
2026-04-11 20:42:40 +0200

    fix: warnings

--- backend/src/main/java/fr/pedalons/service/security/PedalonsQueryContext.java
+++ backend/src/main/java/fr/pedalons/service/security/PedalonsQueryContext.java
@@ -149,6 +149,7 @@ public class PedalonsQueryContext {
     }
   }
 
+  @SuppressWarnings("unchecked")
   @Nullable
   public <T> T getParam(List<Object> params, int i) {
     return (T) params.get(i);
