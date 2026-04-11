21d8d9a26d12adc48d273d252f2db89d01fbf20c
Gabriel Landais <gabriel.landais@gmail.com>
2026-04-11 22:38:10 +0200

    feat: lighter S3 container for tests

--- backend/src/test/java/fr/pedalons/util/MinioTestResource.java
+++ backend/src/test/java/fr/pedalons/util/MinioTestResource.java
@@ -1,53 +1,52 @@
 package fr.pedalons.util;
 
+import com.adobe.testing.s3mock.testcontainers.S3MockContainer;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import java.util.Map;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
 /**
- * Quarkus test resource that starts MinIO and imgproxy containers for storage and image processing
- * tests. Both containers share a Docker network so imgproxy can access MinIO by hostname.
+ * Quarkus test resource that starts S3Mock and imgproxy containers for storage and image processing
+ * tests. Both containers share a Docker network so imgproxy can access S3Mock by hostname.
  */
 public class MinioTestResource implements QuarkusTestResourceLifecycleManager {
 
   private Network network;
-  private MinIOContainer minio;
+  private S3MockContainer s3mock;
   private GenericContainer<?> imgproxy;
 
   @Override
   public Map<String, String> start() {
     network = Network.newNetwork();
 
-    minio =
-        new MinIOContainer("minio/minio:RELEASE.2025-01-20T14-49-07Z")
-            .withUserName("minioadmin")
-            .withPassword("minioadmin")
+    s3mock =
+        new S3MockContainer("latest")
+            .withInitialBuckets("pedalons-test")
             .withNetwork(network)
-            .withNetworkAliases("minio");
-    minio.start();
+            .withNetworkAliases("s3mock");
+    s3mock.start();
 
     imgproxy =
         new GenericContainer<>("ghcr.io/imgproxy/imgproxy:v3.30.1")
             .withNetwork(network)
             .withExposedPorts(8080)
             .withEnv("IMGPROXY_USE_S3", "true")
-            .withEnv("AWS_ACCESS_KEY_ID", "minioadmin")
-            .withEnv("AWS_SECRET_ACCESS_KEY", "minioadmin")
-            .withEnv("IMGPROXY_S3_ENDPOINT", "http://minio:9000")
+            .withEnv("AWS_ACCESS_KEY_ID", "foo")
+            .withEnv("AWS_SECRET_ACCESS_KEY", "bar")
+            .withEnv("IMGPROXY_S3_ENDPOINT", "http://s3mock:9090")
             .withEnv("AWS_REGION", "us-east-1")
             .waitingFor(new HttpWaitStrategy().forPath("/health").forPort(8080));
     imgproxy.start();
 
-    String minioEndpoint = minio.getS3URL();
+    String s3Endpoint = s3mock.getHttpEndpoint();
     String imgproxyUrl = "http://" + imgproxy.getHost() + ":" + imgproxy.getMappedPort(8080);
 
     return Map.of(
-        "quarkus.s3.endpoint-override", minioEndpoint,
-        "quarkus.s3.aws.credentials.static-provider.access-key-id", "minioadmin",
-        "quarkus.s3.aws.credentials.static-provider.secret-access-key", "minioadmin",
+        "quarkus.s3.endpoint-override", s3Endpoint,
+        "quarkus.s3.aws.credentials.static-provider.access-key-id", "foo",
+        "quarkus.s3.aws.credentials.static-provider.secret-access-key", "bar",
         "storage.bucket", "pedalons-test",
         "quarkus.rest-client.imgproxy.url", imgproxyUrl);
   }
@@ -57,8 +56,8 @@ public class MinioTestResource implements QuarkusTestResourceLifecycleManager {
     if (imgproxy != null) {
       imgproxy.stop();
     }
-    if (minio != null) {
-      minio.stop();
+    if (s3mock != null) {
+      s3mock.stop();
     }
     if (network != null) {
       network.close();
