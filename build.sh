8dda6aa45d2753dd9ecbf1b0a0b36dcd1f91a195
Gabriel Landais <gabriel.landais@gmail.com>
2026-07-09 12:38:37 +0200

    chore: image tag based on ENV_NAME

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 export $(grep -v '^#' .env | xargs)
 cd backend
+export QUARKUS_CONTAINER_IMAGE_REGISTRY="ghcr.io"
 export QUARKUS_CONTAINER_IMAGE_GROUP="pedalons"
 export QUARKUS_CONTAINER_IMAGE_NAME="pedalons-backend"
 export QUARKUS_CONTAINER_IMAGE_TAG="$ENV_NAME"
@@ -7,6 +8,6 @@ mvn clean package -DskipTests -Dquarkus.container-image.build=true
 cd ../frontend
 mkdir -p src/assets/legal
 cp ../privacy/*.md src/assets/legal/
-docker build --progress=plain -t pedalons/pedalons-frontend:$ENV_NAME .
+docker build --progress=plain -t ghcr.io/pedalons/pedalons-frontend:$ENV_NAME .
 rm -rf src/assets/legal
 cd ..
